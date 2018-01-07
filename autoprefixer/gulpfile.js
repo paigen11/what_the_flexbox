be0babda223e36d1e219237978285a2db352cb2d
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-01-07 09:26:40 -0500

    flex awesomeness continued

--- autoprefixer/gulpfile.js
+++ autoprefixer/gulpfile.js
@@ -3,12 +3,15 @@ var autoprefixer = require('gulp-autoprefixer');
 
 
 gulp.task('styles',function() {
+  // grab the content of your style...
   gulp.src('css/styles.css')
+  // pipe it into autoprefixer...
     .pipe(autoprefixer())
+      // then pipe it into a build folder
     .pipe(gulp.dest('build'))
 });
 
-
+// this will have gulp watch files and rerun whenever those files change (you can pass an array through this watch)
 gulp.task('watch',function() {
   gulp.watch('css/styles.css', ['styles']);
 });
